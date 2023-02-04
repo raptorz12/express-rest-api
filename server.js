a0e4d4fad8b28179f6664ba3f5ff19bab3379fa4
Jefry Antonius <jefry_plg@yahoo.co.id>
2023-02-04 10:59:46 +0700

    change api endpoint

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 })
 
 const router = require('./app/routes/routes')
-app.use('/api', router)
+app.use('/hero', router)
 
 app.listen(PORT, () => {
   console.log(`App is listening in port ${PORT}`);
